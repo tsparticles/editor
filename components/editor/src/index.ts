1a704bf1884645973d6f53800552bfd36d9bc697
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2023-04-14 15:57:30 +0200

    build: updated deps and build configs

--- components/editor/src/index.ts
+++ components/editor/src/index.ts
@@ -1,6 +1,10 @@
 import type { Container } from "tsparticles-engine";
 import { ParticlesEditor } from "./ParticlesEditor";
 
+/**
+ *
+ * @param container
+ */
 export function showEditor(container: Container): ParticlesEditor {
     return new ParticlesEditor(container);
 }
